65141bc9c90d8c37dba1c98b2e3532bab69ea257
wonjinsong <msmk22239@gmail.com>
2021-02-28 20:17:20 -0500

    plantMine function

--- mineSweeper.jsx
+++ mineSweeper.jsx
@@ -25,6 +25,35 @@ const initialState = {
     result : '',
 };
 
+//지뢰를 심는 함수
+const plantMine = (row, cell, mine) => {
+    const candidate = Array(row * cell).fill().map((arr, i) => {
+        return i;
+    });
+    const shuffle = [];
+    while(candidate.length > row * cell - mine) {
+        const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
+        shuffle.push(chosen);
+
+    }
+    const data = [];
+    for(let i = 0; i <row; i++){
+        const rowData = [];
+        data.push(rowData);
+        for(let j =0 ; j < clearImmediate; j++) {
+            rowData.push(CODE.NORMAL);
+        }
+    }
+
+    for(let k =0; k < shuffle.length; k++) {
+        const ver = Math.floor(shuffle[k] / cell);
+        const hor = shuffle[k] % cell;
+        data[ver][hor] = CODE.MINE;
+    }
+    console.log(data);
+    return data;
+};
+
 export const START_GAME = 'START_GAME';
 
 const reducer = (state, action) => {
