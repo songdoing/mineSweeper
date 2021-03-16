2d7a67010d44338cf9143d9216addda0bf800edc
wonjinsong <msmk22239@gmail.com>
2021-03-16 00:00:27 -0400

    according code

--- td.jsx
+++ td.jsx
@@ -1,5 +1,5 @@
 import React, {useContext, useCallback} from 'react';
-import { CODE, OPEN_CELL, TableContext } from './mineSweeper';
+import { CODE, OPEN_CELL, CLICK_MINE, TableContext } from './mineSweeper';
 
 const getTdStyle = (code) => {
     switch (code) {
@@ -8,9 +8,23 @@ const getTdStyle = (code) => {
             return {
                 background : 'dodgerblue',
             };
+        case CODE.CLICKED_MINE :
+            return {
+                background : 'black',
+            }
         case CODE.OPENED :
             return {
                 background : 'skyblue',
+            };
+        case CODE.QUESTION :
+        case CODE.QUESTION_MINE :
+            return {
+                background : 'yellow',
+            };
+        case CODE.FLAG_MINE :
+        case CODE.FLAG :
+            return {
+                background : 'red',
             };
         default : 
             return {
@@ -25,6 +39,14 @@ const getTdText = (code) => {
             return  '';
         case CODE.MINE :
             return 'X';
+        case CODE.CLICKED_MINE : 
+            return 'B';
+        case CODE.FLAG_MINE :
+        case CODE.FLAG :
+            return '!';
+        case CODE.QUESTION_MINE :
+        case CODE.QUESTION :
+            return '?';
         default :
             return '';    
     }
@@ -34,9 +56,26 @@ const Td = ( { rowIndex, cellIndex }) => {
     const { tableData, dispatch} = useContext(TableContext);
 
     const onClickTd = useCallback(() => {
-        
         // td를 클릭하면, dispatch에 클릭한 정보가 들어가고, MineSweeper에서 정보변경
-        dispatch({ type : OPEN_CELL, row:rowIndex, cell: cellIndex });
+
+        switch (tableData[rowIndex][cellIndex]) {
+            case CODE.OPENED :
+            case CODE.FLAG_MINE :
+            case CODE.FLAG :
+            case CODE.QUESTION_MINE :
+            case CODE.QUESTION :
+                return;
+            case CODE.NORMAL :
+                dispatch({ type : OPEN_CELL, row:rowIndex, cell: cellIndex });
+                return;
+            case CODE.MINE : 
+                dispatch ( { type : CLICK_MINE, row : rowIndex, cell : cellIndex});
+                return;
+            default : 
+                return;
+        }
+        
+        
     }, []);
 
     return (
