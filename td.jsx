1966c4830c792dc7d221cff791ba840d9fe15a8a
wonjinsong <msmk22239@gmail.com>
2021-03-15 23:20:57 -0400

    td style, text

--- td.jsx
+++ td.jsx
@@ -1,10 +1,41 @@
 import React, {useContext} from 'react';
-import { TableContext } from './mineSweeper';
+import { CODE, TableContext } from './mineSweeper';
+
+const getTdStyle = (code) => {
+    switch (code) {
+        case CODE.NORMAL :
+        case CODE.MINE :
+            return {
+                background : 'dodgerblue',
+            };
+        case CODE.OPENED :
+            return {
+                background : 'white',
+            };
+        default : 
+            return {
+                background : 'white',
+            };
+    }
+};
+
+const getTdText = (code) => {
+    switch (code) {
+        case CODE.NORMAL :
+            return  '';
+        case CODE.MINE :
+            return 'X';
+        default :
+            return '';    
+    }
+};
 
 const Td = ( { rowIndex, cellIndex }) => {
     const { tableData} = useContext(TableContext);
     return (
-        <td>{tableData[rowIndex][cellIndex]}</td>
+        <td
+            style = {getTdStyle(tableData[rowIndex][cellIndex])}
+        >{getTdText(tableData[rowIndex][cellIndex])}</td>
     );
 };
 
